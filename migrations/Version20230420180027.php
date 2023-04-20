0e353af4a22d624ccf61fcca92860fef885fb7fc
Adrian Florczak <af_site@outlook.com>
2023-04-20 20:58:28 +0200

    migration file

---
+++ migrations/Version20230420180027.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230420180027 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE branch ADD img_src VARCHAR(255) NOT NULL, ADD nav_link VARCHAR(255) NOT NULL, ADD map_link VARCHAR(500) NOT NULL, ADD email VARCHAR(255) NOT NULL, ADD phone VARCHAR(30) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE branch DROP img_src, DROP nav_link, DROP map_link, DROP email, DROP phone');
+    }
+}
